81fcfbe0dc4b348fa3cf42badde17877564e2722
LuLu <lu.luzan@gmail.com>
2023-09-18 00:55:52 +0200

    render_ok

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,29 +1,5 @@
 <script setup>
-(function(){
-	$('.flex-container').waitForImages(function() {
-		$('.spinner').fadeOut();
-	}, $.noop, true);
-	
-	$(".flex-slide").each(function(){
-		$(this).hover(function(){
-			$(this).find('.flex-title').css({
-				transform: 'rotate(0deg)',
-				top: '10%'
-			});
-			$(this).find('.flex-about').css({
-				opacity: '1'
-			});
-		}, function(){
-			$(this).find('.flex-title').css({
-				transform: 'rotate(90deg)',
-				top: '15%'
-			});
-			$(this).find('.flex-about').css({
-				opacity: '0'
-			});
-		})
-	});
-})();</script>
+</script>
 
 <template>
 <div class="flex-container">
@@ -69,7 +45,7 @@
 <style>
 @import url('https://fonts.googleapis.com/css?family=Raleway');
 
-$defaultSeconds: 3s;
+$defaultSeconds: ;
 
 body {
 	margin: 0;
